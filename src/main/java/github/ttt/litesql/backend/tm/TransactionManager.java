7a3353af78550091f2b8030b3be10022a2f9c8cd
TTT <1794230131@qq.com>
2025-04-18 20:35:52 +0800

    造轮子之LiteSQL正式开始啦

---
+++ src/main/java/github/ttt/litesql/backend/tm/TransactionManager.java
@@ -0,0 +1,13 @@
+package github.ttt.litesql.backend.tm;
+
+public interface TransactionManager {
+    long begin();
+    void commit(long xid);
+    void abort(long xid);
+    boolean isActive(long xid);
+    boolean isCommitted(long xid);
+    boolean isAborted(long xid);
+    void close();
+
+
+}
